fc5e28eef5a3eac980a9a7203668e2f18e8c0821
madeibao <2901429479@qq.com>
2026-08-13 18:56:14 +0800

    add some demo of python

---
+++ python/strdemo.py
@@ -0,0 +1,18 @@
+
+
+def calculate(a, b):
+    """计算两数之和。
+
+    Args:
+        a (int): 第一个数
+        b (int): 第二个数
+
+    Returns:
+        int: 两数之和
+    """
+    return a + b
+
+if __name__ == "__main__":
+    print(calculate(1, 2))
+    print("\n")
+    print(calculate.__doc__)
